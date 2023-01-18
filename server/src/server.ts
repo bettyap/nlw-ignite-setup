c0da182cee1931d1c5f3ed9f7d983eea153f024d
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-01-17 22:59:06 -0300

    add os arquivos server, web

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify ()
+const prisma = new PrismaClient()
+
+/**
+ * Metodo HTTP: Get, Post, Put, Patch, Delete
+ */
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server running!')
+})
