83b60d9782227c18863829457a6d5cace4745ef5
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-01-23 16:12:00 -0300

    aula 05 web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -44,7 +44,7 @@ export function SummaryTable(){
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
+        {summary.length > 0 && summaryDates.map((date) => {
           
           const dayInSummary = summary.find(day => {
             return dayjs(date).isSame(day.date, 'day')
@@ -52,10 +52,10 @@ export function SummaryTable(){
 
           return (
             <HabitDay 
-            key={date.toString()}
-            date={date}
-            amount={dayInSummary?.amount} 
-            completed={dayInSummary?.completed} 
+              key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount} 
+              defaultCompleted={dayInSummary?.completed} 
             />
           )
         })}
